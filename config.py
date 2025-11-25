1324195e1aab03930d2e720a2a9aad7dd05f0fdd
Solo12e <T.sjylalkl@gmail.com>
2025-11-25 20:25:49 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# ============================
+#   CONFIGURATION
+# ============================
+
+BOT_TOKEN = "8452773152:AAEJyOt0N5OxLZ9lBzTaLefKF4_wVu8_oSg"
+OWNER_ID = 8088087792
+
+# Gemini API
+GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
+GEMINI_API_KEY = "AIzaSyDD-ZHKeqXI2ZlMMb1NNFJUSrECTw5YqBQ"
+
+# قاعدة بيانات
+DB_PATH = "bot.db"
+
+# رابط البحث الأساسي
+ANNA_SEARCH = "https://ar.annas-archive.li/search?index=&page=1&sort=&display=&q="
